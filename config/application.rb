4a7eb596bb5f24caa5ed4d4a6908b46a019cc970
mario <github@lxxxvi.ch>
2020-01-10 16:39:56 +0100

    reconfigure discogs token

--- config/application.rb
+++ config/application.rb
@@ -21,17 +21,9 @@ Bundler.require(*Rails.groups)
 
 module Vdb
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
-    config.discogs_token = ENV['DISCOGS_TOKEN']
-    config.discogs_wrapper = Discogs::Wrapper.new('VDB', user_token: config.discogs_token)
-
     config.action_view.form_with_generates_remote_forms = false
     config.settings = config_for(:settings)
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.discogs_wrapper = Discogs::Wrapper.new('VDB', user_token: config.settings.discogs_token)
   end
 end
