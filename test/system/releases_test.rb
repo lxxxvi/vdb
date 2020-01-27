a133239f929e5b9914fc64d69e7cf6efaeb1a642
mario <github@lxxxvi.ch>
2020-01-27 17:50:48 +0100

    add simple test

--- test/system/releases_test.rb
+++ test/system/releases_test.rb
@@ -1,7 +1,7 @@
 require 'application_system_test_case'
 
 class ReleasesTest < ApplicationSystemTestCase
-  test 'creates a new release without Discogs ID' do
+  test 'creates a new release without Discogs Release ID' do
     sign_in_as(:pete)
 
     visit library_path
@@ -17,6 +17,22 @@ class ReleasesTest < ApplicationSystemTestCase
       fill_in 'Year', with: '1970'
       fill_in 'Genre', with: 'Dixie'
       fill_in 'Format quantity', with: '1'
+
+      click_on 'Add release'
+    end
+
+    assert_selector '.flash-success', text: 'Release created'
+  end
+
+  test 'creates a new release using Discogs Relase ID' do
+    sign_in_as(:pete)
+
+    visit library_path
+    click_on 'Add release'
+    assert_selector 'h1', text: 'Add release'
+
+    within('.discogs-release-id-form') do
+      fill_in 'Discogs Relase ID', with: 6990537
 
       click_on 'Add release'
     end
